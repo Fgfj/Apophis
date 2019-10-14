b1b3d206faf43e99b5afab77faa3a960673d94d7
13683498647@163.com <123456yu>
2019-10-14 15:57:38 +0800

    test

---
+++ apophislib/src/main/java/com/yz/apophislib/test/ApopHisLibTest.java
@@ -0,0 +1,14 @@
+package com.yz.apophislib.test;
+
+import android.util.Log;
+
+/**
+ * author:yz
+ * data: 2019/10/14,15:56
+ */
+public class ApopHisLibTest {
+    public static final String LOG="A_POP_HIS_LIST_TEST_LOG";
+    public static void toTestInit(){
+        Log.i("A_POP_HIS_LIST_TEST_LOG","toTestInit");
+    }
+}
